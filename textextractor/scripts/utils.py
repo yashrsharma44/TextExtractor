eca71650cffec950fb735dacfb131943abfd6cf6
Yash <yashrsharma44@gmail.com>
2018-12-28 16:26:50 +0530

    Added support for fpdf

---
+++ textextractor/scripts/utils.py
@@ -0,0 +1,122 @@
+import requests
+import time
+
+import matplotlib.pyplot as plt
+from matplotlib.patches import Polygon
+from PIL import Image
+from io import BytesIO
+
+def _get_subscription():
+    """ 
+    Function to get the subscription key from the user \
+    for the Microsoft Azure account
+    
+    Returns:
+        String -- Subscription key accepted from user
+    """
+
+    subscription_key = str(input("Enter the Subscription key :"))
+    return subscription_key
+
+def _API_url():
+    """
+    Function to get the url for the main API
+
+    Returns:
+        String -- API url
+    """
+
+    vision_base_url = "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/"
+    text_recognition_url = vision_base_url + "recognizeText"
+    return(text_recognition_url)
+
+def make_request(image_url):
+    """
+    Makes the request for the image collected for performing text detection
+    Uses Microsoft's Azure for getting the text
+    More info : "https://azure.microsoft.com/en-in/services/cognitive-services/computer-vision/"
+
+    Arguments:
+        image_url {String} -- url of the image stored
+    
+    Returns:
+        [Object] -- Response received
+        [String] -- Operation url from response
+
+    """
+    subscription_key = _get_subscription()
+
+    headers = {'Ocp-Apim-Subscription-Key': subscription_key}
+    params  = {'mode': 'Printed'}
+    data    = {'url': image_url}
+    text_recognition_url = _API_url()
+
+    response = requests.post(text_recognition_url, headers=headers,\
+    params=params, json=data)
+    response.raise_for_status()
+    operation_url = response.headers["Operation-Location"]
+
+    return (response, operation_url)
+
+
+def get_text(analysis):
+    """
+    Gets the resultant text of the image supplied
+    
+    Arguments:
+        analysis {JSON} -- JSON form of the final response 
+        that contains the text detected
+    
+    Returns:
+        [list] -- Returns the polygon data, and the text detected 
+    """
+
+
+    if('recognitionResult' in analysis):
+        polygons = [(line["boundingBox"], line["text"])for line in analysis["recognitionResult"]["lines"]]
+        text = [line['text'] for line in analysis["recognitionResult"]["lines"]]
+        return (polygons, text)
+    else:
+        return('Sorry the image that you sent cannot be \
+        recognised! Please try with another image.')
+
+
+def display_image(image_url, plt, polygons):
+    """
+    Generates the polygon plots for the text extracted,
+    and maps out the extracted text in the image itself
+    
+    Arguments:
+        image_url {String} -- Url of the image
+        plt {Object} -- Plot object
+        polygons {list} -- Contains the x,y coordinates of the text extracted
+
+    Returns:
+        plt {Object} : Plot object
+    """
+
+    plt.figure(figsize=(15, 15))
+    image = Image.open(BytesIO(requests.get(image_url).content))
+
+    ax = plt.imshow(image)
+
+    for polygon in polygons:
+	    vertices = [(polygon[0][i], polygon[0][i+1])
+		for i in range(0, len(polygon[0]), 2)]
+	    text     = polygon[1]
+	    patch    = Polygon(vertices, closed=True, fill=False, linewidth=2, color='y')
+	    ax.axes.add_patch(patch)
+	    plt.text(vertices[0][0], vertices[0][1], text, fontsize=20, va="top")
+
+    _ = plt.axis("off")
+
+    return(plt)
+
+
+
+
+
+
+
+
+
